e19619f23336905404f9849ed5c8d4747aa69919
sormys <szymonp1806@gmail.com>
2023-07-14 17:00:38 +0200

    added style to autocomplete

--- simple-forecast/src/components/search/index.js
+++ simple-forecast/src/components/search/index.js
@@ -1,10 +1,45 @@
-import React from "react"
+import React, { useContext } from "react"
 import { useDispatch, useSelector } from "react-redux"
 import Select from "react-select"
 import { changeCurrent } from "../weather/reducer"
 import { changeSearch } from "./reducer"
 import { AutocompleteBarWrapper } from "./components"
 import { LineLoader } from "../loaders"
+import styled from "styled-components"
+import { ThemeContext } from "styled-components"
+
+const getCustomStyles = (theme) => {
+  const { primary, background, foreground, selection, text } = theme;
+  return {
+    control: (provided, state) => ({
+      ...provided,
+      color: selection,
+      backgroundColor: state.isFocused ? text : background,
+      borderColor: state.isFocused ? primary : background,
+      boxShadow: state.isFocused ? `0 0 0 1px ${primary}` : "none",
+      "&:hover": {
+        borderColor: state.isFocused ? primary : background,
+      },
+    }),
+    option: (provided, state) => ({
+      ...provided,
+      backgroundColor: state.isFocused ? primary : selection,
+      color: state.isFocused ? text : foreground,
+    }),
+  };
+};
+
+const StyledSelect = styled(Select)`
+  .react-select__control {
+    border-radius: 0;
+    border: none;
+    box-shadow: none;
+  }
+  .react-select__menu {
+    border-radius: 0;
+    margin-top: 0;
+  }
+`
 
 const AutocompleteBar = () => {
   const results = useSelector((state) => state.search.results)
@@ -34,15 +69,20 @@ const AutocompleteBar = () => {
     }))
   }
 
+  const theme = useContext(ThemeContext)
+  const customStyles = getCustomStyles(theme.primary, theme.background, theme.foreground, theme.selection, theme.text)
+    
+
   return (
     <AutocompleteBarWrapper>
-      <Select
+      <StyledSelect
         options={options}
         onInputChange={handleInputChange}
         onChange={handleAutocompleteResultChange}
         isLoading={isLoading}
         loadingMessage={() => <LineLoader />}
         placeholder="Search for a city..."
+        styles={customStyles}
       />
     </AutocompleteBarWrapper>
   )
